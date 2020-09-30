8ac46cd45115091697dfc24d5e309e7c62665f3e
yayayavuz <yahyayavuz1@gmail.com>
2020-10-01 01:39:51 +0300

    ETL ready

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,38 @@
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages,categories,on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(pat=';',expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.str.split('-',expand=True)[0]
+    categories.columns = category_colnames
+
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+
+    # convert column from string to numeric
+        categories[column] = categories[column].astype('int64')
+    df = df.drop(['categories'], axis=1)
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('InsertTableName', engine, chunksize=1000, index=False,if_exists='replace')
+
 
 
 def main():
@@ -24,12 +46,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
