6114641fc06e5fbdf2df01fdf71ea7820ad9411d
yayayavuz <yahyayavuz1@gmail.com>
2020-10-02 09:20:59 +0300

    ML Pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,57 @@
 import sys
-
+from sqlalchemy import create_engine
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('InsertTableName', engine)
+    X = df['message']
+    Y = df.drop(columns=['id', 'original', 'genre', 'message'], axis=1)
+    category_names = Y.columns
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    tokens = nltk.word_tokenize(text)
+    tokens = [WordNetLemmatizer().lemmatize(word) for word in tokens if word not in stopwords.words("english")]
+
+    return tokens
 
 
 def build_model():
-    pass
+        pipeline = Pipeline([
+                    ('vect', CountVectorizer(tokenizer=tokenize)),
+                    ('tfidf', TfidfTransformer()),
+                    ('classifier', MultiOutputClassifier(RandomForestClassifier()))
+                    ])
+        parameters = {
+            'classifier__estimator__n_estimators': [20, 30]
+        }
+
+        cv = GridSearchCV(pipeline, param_grid=parameters)
+        return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    df_y_pred = pd.DataFrame(data=y_pred, columns=category_names)
+    for col in category_names:
+        print(col,classification_report(Y_test[col], df_y_pred[col], labels=[0, 1]))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -26,14 +59,14 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
